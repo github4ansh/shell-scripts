3cce8fa4ec632dd51b53abe91c89c2258b699001
github4ansh <deianshagarwal@gmail.com>
2022-10-08 18:35:24 -0400

    inital commit

--- variables.sh
+++
@@ -1,9 +0,0 @@
-echo "Hello World"
-
-WORD='Ansh!' # variable names can not start with number
-
-echo "Way 1 (accessing variable in double quotes! it worked):$WORD" # displaying value of the variable
-
-echo 'Way 2 (accessing variable in single quote! does not work): $WORD' # single quote doesn't recognize a variable
-
-echo "Way 3 (accessing variable having variable name enclosed in curly braces and prefixed with $ sign): ${WORD}" # alternative way to access variable 
